22b6ff0c0cdfbb4f901b7f122738ee8d61671d5d
DeveloperVlad <lp.es@yandex.ru>
2022-01-24 12:12:12 +0500

    lesson-2 sayUsers end

---
+++ lesson-2sayusers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+//? Alert
+let user = 'Vlad'
+// alert('Привет ' + `${user}`);
+
+//? Confirm
+// Записывает значение, если пользователь нажимает на Ok(true), если жмёт отмена то (false);
+// const result = confirm('Привет как твои дела ' + `${user}`);
+
+//? Promt 
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(typeof(answer)); // Пападает всегда строка, но если поставить +перед prompt, то будет выводить всегда number(Преобразование)
+
+const answers = [];
+answers[0] = prompt('Привет как тебя зовут? ', '');
+answers[1] = prompt('Сколько тебе лет? ', '');
+answers[2] = prompt('Какая у тебя фамилия? ', '');
+document.write(answers);
+console.log(answers);
+
+
