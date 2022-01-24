00a27919bdffcddf379a21fe90670d0bce0efb92
DeveloperVlad <lp.es@yandex.ru>
2022-01-24 11:33:34 +0500

    first lesson end type

---
+++ lesson-1type/main.js
@@ -0,0 +1,61 @@
+//? Типы данных
+
+// Числа - 1,2,3
+// Строки - 'privet'
+// Логический тип - (bolean) = true || false
+// Null - означает, что значение не существует 
+// undefined - значение есть, но не определенно
+// BigInt - тип данных, который отображает большие числа
+
+//? Комлексные типы данных
+// Спец объекты 
+// [] 
+// function () {}
+// Обьекты даты 
+// Регулярные выражение 
+// Ошибки 
+// Объекты коллекция хранящих в себе данные
+
+// Примеры 
+'use scrict'
+// Числа
+let number = 4;
+console.log(-4/0);  //js позволяет так делать
+console.log('string' * 9); // NaN
+
+// Строки
+const person = 'vlad';
+
+// Логический тип данных boolean
+const bool = true; // Да
+const bool1 = false // Нет 
+
+// Null
+// console.log(something); // Не существующий объект
+
+// Undefiend
+let und;
+// console.log(und); // Объект сущесвует, но в нем ничего нет.
+
+// Объекты 
+const obj = {
+    name: 'vlad',
+    age: 23,
+    isWeb: true,
+}
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.isWeb);
+
+// Массивы 
+let arr = ['0', '1', '2', '3'];
+
+console.log(arr[1]);
+
+
+
+
+
+
+
+
