8c61a71ed0f75db358cfdf403ba74813315be64c
DeveloperVlad <lp.es@yandex.ru>
2022-02-27 14:36:41 +0500

    Получение элементов со страницы

---
+++ lesson-17DOMelements/main.js
@@ -0,0 +1,17 @@
+'use strict';
+// Получение по ID 
+const box = document.getElementById('box');
+
+// Получение всех элементов на странице по тэгу button
+const buttons = document.getElementsByTagName('button');
+
+// Получение всех элементов по классу 
+const circle = document.getElementsByClassName('circle');
+
+
+// И querySelector и querySelectorALl
+
+const heartOne = document.querySelector('.heart');
+
+const heart = document.querySelectorAll('.heart');
+
